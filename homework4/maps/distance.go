2a4b92f399bc4eb4c44b2f2f69ea7e3bb62d84c6
dantedenis <76144861+dantedenis@users.noreply.github.com>
2022-02-17 08:18:07 +0300

    Update distance.go
    add method "AddPointSlice/AddPoint" , delete Inteface

--- homework4/maps/distance.go
+++ homework4/maps/distance.go
@@ -1,9 +1,5 @@
 package maps
 
-type Coordinate interface {
-	Distance() float64
-}
-
 type Arc struct {
 	start PointPlanet
 	end   PointPlanet
@@ -11,10 +7,31 @@ type Arc struct {
 	name  string
 }
 
+type ArcSlice struct {
+	arcs []Arc
+}
+
 func NewArc(p1, p2 PointPlanet) *Arc {
 	return &Arc{start: p1, end: p2, long: p1.Distance(p2)}
 }
 
 func (a Arc) Distance() float64 {
-	return a.start.Distance(a.end)
+	return a.long
+}
+
+func (a Arc) AddPoint(points ...PointPlanet) result *ArcSlice {
+	result = append(result, Arc)
+	for _, point range points {
+		indexLast := len(result) - 1
+		result = append(result, NewArc(result[indexLast], point))
+	}
+	return &result
+}
+
+func (aSlice *ArcSlice) AddPointSlice(points ...PointPlanet) *ArcSlice {
+	for _, point range points {
+		indexLast := len(aSlice.arcs) - 1
+		aSlice.arcs = append(aSlice.arcs, NewArc(aSlice.arcs[indexLast] ,point))
+	}
+	return aSlice
 }
